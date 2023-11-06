41db6644a5af8799adedd67f61f065b9ebd42c0f
devurqui <devurqui@gmail.com>
2023-11-06 15:07:03 +0100

    Añadido Model

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,7 +1,12 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -9,9 +14,24 @@ public class IndexController {
 	/*por defecto es un metodo GET, si entras por https://localhost:8080/index*/
 	/*Se puede utilizar GetMapping obviando el method, o PostMapping, DeleteMapping...RequestMapping pero hay que importarlo*/
 	/*Retorna el template inicial que se guarda en /resources/templates*/
-	@GetMapping({"/index","/","/home"})
-	public String index() {
-		
+	/*@GetMapping({"/index","/","/home"})
+	public String index(Model model) { 
+		model.addAttribute("titulo","Hola Spring Framework con Map!");
 		return "index";
+	@GetMapping({"/index","/","/home"})
+	public String index(ModelMap model) {
+		model.addAttribute("titulo","Hola Spring Framework con ModelMap!");
+		return "index";
+	}
+	@GetMapping({"/index","/","/home"})
+	public String index(Map<String, Object> map) { 
+		map.put("titulo","Hola Spring Framework con Map!");
+		return "index";
+	}*/
+	@GetMapping({"/index","/","/home"})
+	public ModelAndView index(ModelAndView mv) { 
+		mv.addObject("titulo","Hola Spring Framework con ModelAndView!");
+		mv.setViewName("index"); //Selecciona el nombre de la vista
+		return mv;
 	}
 }
